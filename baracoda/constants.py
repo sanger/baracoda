64c13e3ade74d085ac435e1a860c2c8de2c347ce
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-05-12 17:02:08 +0100

    Add constants

---
+++ baracoda/constants.py
@@ -0,0 +1,7 @@
+from typing import Final
+
+# Prefix Config keys
+CONFIG_ENABLE_CHILDREN_CREATION: str = "enableChildrenCreation"
+CONFIG_PREFIX: str = "prefix"
+CONFIG_SEQUENCE_NAME: str = "sequence_name"
+CONFIG_FORMATTER_CLASS: str = "formatter_class"
