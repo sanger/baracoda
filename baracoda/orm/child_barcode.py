18678f90c501aa5398b93d1fb3b4137dee9e5010
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-05-13 16:31:00 +0100

    More changes

--- baracoda/orm/child_barcode.py
+++ baracoda/orm/child_barcode.py
@@ -3,6 +3,11 @@ from baracoda.orm.base import Base
 
 
 class ChildBarcode(Base):
+    """Class that will define the ORM mapping with the database. It will
+    store a list of all parent barcodes with an index of the last created
+    child for the parent for each entry.
+    """
+
     __tablename__ = "child_barcode_counter"
 
     barcode = Column(String(50), nullable=False, primary_key=True)
