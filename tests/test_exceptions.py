a269b69c3edad8cc28090bcccb2f18ccc63ceb70
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-06-07 14:37:11 +0100

    Test exception classes

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,15 +1,22 @@
 import pytest
-from baracoda.exceptions import UnsupportedEncodingForPrefix, InvalidBarcodeError, InvalidCountError, UnsupportedChildrenCreation, UnsupportedChildrenCreation, InvalidPrefixError, InvalidCountError, InvalidBarcodeError
+from baracoda.exceptions import (
+    UnsupportedEncodingForPrefix,
+    UnsupportedChildrenCreation,
+    InvalidPrefixError,
+    InvalidCountError,
+    InvalidBarcodeError,
+)
 
 EXCEPTIONS = [
     UnsupportedEncodingForPrefix,
     UnsupportedChildrenCreation,
     InvalidPrefixError,
     InvalidCountError,
-    InvalidBarcodeError
+    InvalidBarcodeError,
 ]
+
 
 @pytest.mark.parametrize("exception_class", EXCEPTIONS)
 def test_exception_str(exception_class):
-    msg = 'testing message'
+    msg = "testing message"
     assert msg in str(exception_class(msg))
