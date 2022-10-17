c1cb81c66ee5e44ec7376dcf599513cef51649e2
Sangeetha <sb67@sanger.ac.uk>
2022-10-17 11:05:22 +0100

    Fix for flask_migrate error, merged two heads bcd74f0a02ea and e47fde01cc62

---
+++ migrations/versions/20c4410755ec_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: 20c4410755ec
+Revises: bcd74f0a02ea, e47fde01cc62
+Create Date: 2022-10-17 11:01:13.594526
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "20c4410755ec"
+down_revision = ("bcd74f0a02ea", "e47fde01cc62")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
